b3444efd5574fe6ab88c970a708629b032c914d5
Sakshi Ravindra Darandale <sakshidarandale8@gmail.com>
2026-04-17 00:05:59 +0530

    Time: 2 ms (8.30%) | Memory: 43.8 MB (5.98%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String toLowerCase(String s) 
+    {
+
+        String result = null;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            result = s.toLowerCase();
+        }
+
+        return result;
+        
+    }
+}
